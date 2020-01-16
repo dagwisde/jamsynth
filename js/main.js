9eea64d544d6e07db2d90a8a6795801f60b432fc
Tamarind Joy <joytamarind@gmail.com>
2020-01-16 16:11:02 -0600

    Add visualization

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ const wavePicker = document.querySelector("#waveformPicker");
 const filterFreq = document.querySelector("#filterFreq");
 const filterQ = document.querySelector("#filterQ");
 const filterPicker = document.querySelector("#filterPicker");
+let canvas = document.querySelector("#visualizer");
+let canvasCtx = canvas.getContext("2d");
 
 // Create audio context
 let context = new (window.AudioContext || window.webkitAudioContext)();
@@ -31,17 +33,76 @@ let osc3 = context.createOscillator();
 osc3.type = wavePicker.value;
 osc3.frequency.value = 391.995;
 
+/*================
+    Visualizer
+==================*/
+// Create analyser node
+let analyser = context.createAnalyser();
+// Capture audio data using specific frequency domain
+analyser.fftSize = 2048;
+// Set buffer length
+let bufferLength = analyser.frequencyBinCount;
+// Create a new array to receive audio data
+let dataArray = new Uint8Array(bufferLength);
+
+function draw() {
+  // Keep looping draw function
+  let drawVisual = requestAnimationFrame(draw);
+  // Copy time data to array
+  analyser.getByteTimeDomainData(dataArray);
+  // Canvas fill color
+  canvasCtx.fillStyle = "rgb(200, 200, 200)";
+  canvasCtx.fillRect(0, 0, 500, 150);
+  // Set width and style of stroke
+  canvasCtx.lineWidth = 2;
+  canvasCtx.strokeStyle = "rgb(0, 0, 0)";
+  canvasCtx.beginPath();
+  // Width of each line segment
+  let sliceWidth = (500 * 1.0) / bufferLength;
+  // Position to draw each segment of the line from
+  let x = 0;
+  // Loop through buffer, defining each segment of the waveform
+  for (let i = 0; i < bufferLength; i++) {
+    let v = dataArray[i] / 128.0;
+    let y = (v * 150) / 2;
+
+    if (i === 0) {
+      canvasCtx.moveTo(x, y);
+    } else {
+      canvasCtx.lineTo(x, y);
+    }
+
+    x += sliceWidth;
+  }
+  canvasCtx.lineTo(canvas.width, canvas.height / 2);
+  canvasCtx.stroke();
+}
+
+// Begin drawing
+draw();
+
+/*================
+    Routing
+==================*/
+
 // Connect oscillators to filter node
 osc1.connect(filter);
 osc2.connect(filter);
 osc3.connect(filter);
 
-// Connect filter to gain node
-filter.connect(gainNode);
+// Connect filter to analyser node
+filter.connect(analyser);
+
+// Connect analyser node to gain
+analyser.connect(gainNode);
 
 // Connect gain to destination (speakers/output) & volume
 gainNode.connect(context.destination);
 gainNode.gain.value = volumeControl.value;
+
+/*==================
+    Gain & Waveform
+====================*/
 
 volumeControl.addEventListener("input", () => {
   gainNode.gain.value = volumeControl.value;
@@ -53,6 +114,10 @@ wavePicker.addEventListener("input", () => {
   osc2.type = wavePicker.value;
   osc3.type = wavePicker.value;
 });
+
+/*================
+    Filters 
+==================*/
 
 // Filter selection
 filterPicker.addEventListener("change", () => {
